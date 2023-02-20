706de0cc90f16922e7206bac210da7d0615d8e6a
mehdi salimi <mehdisalimi124@gmail.com>
2023-02-20 14:52:55 -0500

    completed sign and verify private key

--- crypto/keypair_test.go
+++ crypto/keypair_test.go
@@ -1,15 +1,34 @@
 package crypto
 
 import (
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestGeneratePrivateKey(t *testing.T) {
+func TestKeyPairSignVerifySuccess(t *testing.T) {
 	privKey := GeneratePrivateKey()
 	pubKey := privKey.PublicKey()
 
-	address := pubKey.Address()
+	//address := pubKey.Address()
+	msg := []byte("Hello world")
+	sig, err := privKey.Sign(msg)
+	assert.Nil(t, err)
 
-	fmt.Println("address", address)
+	res := sig.Verify(pubKey, msg)
+	assert.True(t, res)
+}
+
+func TestKeyPairSignVerifyFail(t *testing.T) {
+	privKey := GeneratePrivateKey()
+
+	msg := []byte("Hello world")
+	sig, err := privKey.Sign(msg)
+	assert.Nil(t, err)
+
+	otherPrivKey := GeneratePrivateKey()
+	otherPubKey := otherPrivKey.PublicKey()
+
+	assert.False(t, sig.Verify(otherPubKey, msg))
+	assert.False(t, sig.Verify(otherPubKey, []byte("fail test")))
 }
