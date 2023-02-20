2ec7bc1465262018856263961f92a971574322a4
mehdi salimi <mehdisalimi124@gmail.com>
2023-02-20 14:20:17 -0500

    completed generate public and private keys and address

---
+++ crypto/keypair_test.go
@@ -0,0 +1,15 @@
+package crypto
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGeneratePrivateKey(t *testing.T) {
+	privKey := GeneratePrivateKey()
+	pubKey := privKey.PublicKey()
+
+	address := pubKey.Address()
+
+	fmt.Println("address", address)
+}
