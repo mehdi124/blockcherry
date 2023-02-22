e9837c6a928b289b51198925f6287facc4884ddb
mehdi salimi <mehdisalimi124@gmail.com>
2023-02-22 14:54:55 -0500

    add mutex to block

--- core/blockchain.go
+++ core/blockchain.go
@@ -2,12 +2,14 @@ package core
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/sirupsen/logrus"
 )
 
 type Blockchain struct {
 	store     Storage
+	lock      sync.RWMutex
 	headers   []*Header
 	validator Validator
 }
@@ -45,22 +47,32 @@ func (bc *Blockchain) GetHeader(height uint32) (*Header, error) {
 		return nil, fmt.Errorf("given height (%d) is too high", height)
 	}
 
+	bc.lock.Lock()
+	defer bc.lock.Unlock()
+
 	return bc.headers[height], nil
 }
 
 // [0,1,2,3] len is 4
 // [0,1,2,3] height is 3
 func (bc *Blockchain) Height() uint32 {
+
+	bc.lock.RLock()
+	defer bc.lock.RUnlock()
+
 	return uint32(len(bc.headers) - 1)
 }
 
 func (bc *Blockchain) addBlockWithoutValidation(b *Block) error {
+
+	bc.lock.Lock()
+	bc.headers = append(bc.headers, b.Header)
+	bc.lock.Unlock()
 
 	logrus.WithFields(logrus.Fields{
 		"height": b.Height,
 		"hash":   b.Hash(BlockHasher{}),
 	}).Info("adding new block")
 
-	bc.headers = append(bc.headers, b.Header)
 	return bc.store.Put(b)
 }
